9d51e6bbf0a23990cdad520945949ce85d91fc32
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-26 21:13:58 +0300

    Create ManagerController

---
+++ SalesStatistics/WebUI/Controllers/ManagerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Common.Models;
+using Application.UseCases.Managers.Queries.GetManagersWithPagination;
+
+namespace WebUI.Controllers
+{
+    public class ManagerController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<PaginatedList<ManagerDto>>> ManagersPage(
+            [FromQuery] GetManagersWithPaginationQuery query)
+        {
+            return View(await Mediator.Send(query));
+        }
+    }
+}
