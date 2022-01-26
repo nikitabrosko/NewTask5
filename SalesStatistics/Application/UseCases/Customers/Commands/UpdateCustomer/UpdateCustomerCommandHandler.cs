1a62871772d3b5b782abd24b0a156a009543ca5f
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-26 17:44:33 +0300

    Create UpdateCustomerCommand use case

---
+++ SalesStatistics/Application/UseCases/Customers/Commands/UpdateCustomer/UpdateCustomerCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.UseCases.Customers.Commands.UpdateCustomer
+{
+    public class UpdateCustomerCommandHandler : IRequestHandler<UpdateCustomerCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateCustomerCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Customers
+                .FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Customer), request.Id);
+            }
+
+            entity.FirstName = request.FirstName;
+            entity.LastName = request.LastName;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ SalesStatistics/Application/UseCases/Customers/Commands/UpdateCustomer/UpdateCustomerCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.UseCases.Customers.Commands.UpdateCustomer
+{
+    public class UpdateCustomerCommandValidator : AbstractValidator<UpdateCustomerCommand>
+    {
+        public UpdateCustomerCommandValidator()
+        {
+            RuleFor(v => v.FirstName)
+                .MaximumLength(20)
+                .NotEmpty();
+
+            RuleFor(v => v.LastName)
+                .MaximumLength(30)
+                .NotEmpty();
+        }
+    }
+}
