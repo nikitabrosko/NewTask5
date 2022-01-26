67ef4dfc0691f55f036ab8a55c737a6a9814ac52
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-27 00:05:03 +0300

    Create new ForeignKeyDeletionException class

---
+++ SalesStatistics/Application/Common/Exceptions/ForeignKeyDeletionException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Application.Common.Exceptions
+{
+    public class ForeignKeyDeletionException : Exception
+    {
+        public ForeignKeyDeletionException() 
+            : base()
+        {
+            
+        }
+
+        public ForeignKeyDeletionException(string message)
+            : base(message)
+        {
+
+        }
+
+        public ForeignKeyDeletionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+
+        public ForeignKeyDeletionException(string entity, object foreignKey)
+            : base($"Entity \"{entity}\" cannot be deleted, because entity ({foreignKey}) depends on it!")
+        {
+
+        }
+    }
+}
