2ea28cdacb1ffe927f9b729b4b9dd4db236808f0
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-27 00:07:29 +0300

    Create DeleteManagerCommand use case

---
+++ SalesStatistics/Application/UseCases/Managers/Commands/DeleteManager/DeleteManagerCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Managers.Commands.DeleteManager
+{
+    public class DeleteManagerCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ SalesStatistics/Application/UseCases/Managers/Commands/DeleteManager/DeleteManagerCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.UseCases.Managers.Commands.DeleteManager
+{
+    public class DeleteManagerCommandHandler : IRequestHandler<DeleteManagerCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteManagerCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteManagerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Managers
+                .FindAsync(new object[] {request.Id}, cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Manager), request.Id);
+            }
+
+            var ordersCount = _context.Orders
+                .Count(o => o.Manager.Id.Equals(request.Id));
+
+            if (ordersCount > 0)
+            {
+                throw new ForeignKeyDeletionException(nameof(Manager), nameof(Order));
+            }
+
+            _context.Managers.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
