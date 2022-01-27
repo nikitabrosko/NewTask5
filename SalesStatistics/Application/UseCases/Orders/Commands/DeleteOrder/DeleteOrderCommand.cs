2df4cf14009962a75fcddb6ddba1abecd4a1aef4
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-27 15:56:28 +0300

    Create DeleteOrderCommand use case

---
+++ SalesStatistics/Application/UseCases/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ SalesStatistics/Application/UseCases/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.UseCases.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteOrderCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Orders
+                .FindAsync(new object[] {request.Id}, cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            _context.Orders.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
