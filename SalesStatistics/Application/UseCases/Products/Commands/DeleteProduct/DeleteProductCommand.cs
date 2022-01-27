77e2a6081a1b264a1cbfa3bceee697dcdd976cdd
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-27 14:20:18 +0300

    Create DeleteProductCommand use case

---
+++ SalesStatistics/Application/UseCases/Products/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Products.Commands.DeleteProduct
+{
+    public class DeleteProductCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ SalesStatistics/Application/UseCases/Products/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Products.Commands.DeleteProduct
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteProductCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Products
+                .FindAsync(new object[] {request.Id}, cancellationToken);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Product), request.Id);
+            }
+
+            var orderCount = await _context.Orders
+                .CountAsync(o => o.Product.Id
+                    .Equals(request.Id), cancellationToken);
+
+            if (orderCount > 0)
+            {
+                throw new ForeignKeyDeletionException(nameof(Product), nameof(Order));
+            }
+
+            _context.Products.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
