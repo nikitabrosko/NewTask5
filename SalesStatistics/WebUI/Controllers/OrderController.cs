07dee77246eac6c58b14605df3d4a97d503f342d
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-27 15:40:02 +0300

    Update OrderController

--- SalesStatistics/WebUI/Controllers/OrderController.cs
+++ SalesStatistics/WebUI/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Application.Common.Models;
+using Application.UseCases.Orders.Commands.CreateOrder;
+using Application.UseCases.Orders.Commands.UpdateOrder;
 using Application.UseCases.Orders.Queries.GetOrdersWithPagination;
 
 namespace WebUI.Controllers
@@ -12,6 +14,35 @@ namespace WebUI.Controllers
             [FromQuery] GetOrdersWithPaginationQuery query)
         {
             return View(await Mediator.Send(query));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create([FromForm] CreateOrderCommand command)
+        {
+            await Mediator.Send(command);
+
+            return RedirectToAction("OrdersPage");
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Update([FromRoute] int id)
+        {
+            return View(new UpdateOrderCommand { Id = id });
+        }
+
+        [HttpPost("{command}")]
+        [Route("Update/{command}")]
+        public async Task<IActionResult> Update([FromForm] UpdateOrderCommand command)
+        {
+            await Mediator.Send(command);
+
+            return RedirectToAction("OrdersPage");
         }
     }
 }
