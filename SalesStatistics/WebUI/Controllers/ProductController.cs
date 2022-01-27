845a22b1204a26c8942263b7bc8fffbd1b218326
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-27 13:40:55 +0300

    Create ProductController

---
+++ SalesStatistics/WebUI/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Application.Common.Models;
+using Application.UseCases.Products.Queries.GetProductsWithPagination;
+
+namespace WebUI.Controllers
+{
+    public class ProductController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<PaginatedList<ProductDto>>> ProductsPage(
+            [FromQuery] GetProductsWithPaginationQuery query)
+        {
+            return View(await Mediator.Send(query));
+        }
+    }
+}
