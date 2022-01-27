57562453921e0243343de25aadd87db749b5dd71
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-27 14:49:13 +0300

    Create OrderController

---
+++ SalesStatistics/WebUI/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Application.Common.Models;
+using Application.UseCases.Orders.Queries.GetOrdersWithPagination;
+
+namespace WebUI.Controllers
+{
+    public class OrderController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<PaginatedList<OrderDto>>> OrdersPage(
+            [FromQuery] GetOrdersWithPaginationQuery query)
+        {
+            return View(await Mediator.Send(query));
+        }
+    }
+}
