84543c43be0364a84614a8ae3f27bd9d62d0fe91
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-27 14:09:07 +0300

    Update ProductController

--- SalesStatistics/WebUI/Controllers/ProductController.cs
+++ SalesStatistics/WebUI/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Application.Common.Models;
+using Application.UseCases.Products.Commands.CreateProduct;
+using Application.UseCases.Products.Commands.UpdateProduct;
 using Application.UseCases.Products.Queries.GetProductsWithPagination;
 
 namespace WebUI.Controllers
@@ -12,6 +14,35 @@ namespace WebUI.Controllers
             [FromQuery] GetProductsWithPaginationQuery query)
         {
             return View(await Mediator.Send(query));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create([FromForm] CreateProductCommand command)
+        {
+            await Mediator.Send(command);
+
+            return RedirectToAction("ProductsPage");
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Update([FromRoute] int id)
+        {
+            return View(new UpdateProductCommand { Id = id });
+        }
+
+        [HttpPost("{command}")]
+        [Route("Update/{command}")]
+        public async Task<IActionResult> Update([FromForm] UpdateProductCommand command)
+        {
+            await Mediator.Send(command);
+
+            return RedirectToAction("ProductsPage");
         }
     }
 }
