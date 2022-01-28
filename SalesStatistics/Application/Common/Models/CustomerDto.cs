1e9f6a21defddc3d789583e980c6cc8bf9a7d840
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-28 23:53:17 +0300

    Move CustomerDto to Models folder

---
+++ SalesStatistics/Application/Common/Models/CustomerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Common.Models
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
