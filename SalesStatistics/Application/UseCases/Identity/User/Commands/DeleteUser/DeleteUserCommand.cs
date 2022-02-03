b5b292dad9e61224825e7374e11b40529d3b960d
Nik.Brosko <Nikita.brosko@mail.ru>
2022-02-03 23:52:36 +0300

    Create DeleteUserCommand use case

---
+++ SalesStatistics/Application/UseCases/Identity/User/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.UseCases.Identity.User.Commands.DeleteUser
+{
+    public class DeleteUserCommand : IRequest
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ SalesStatistics/Application/UseCases/Identity/User/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.User.Commands.DeleteUser
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
+    {
+        private readonly UserManager<Domain.IdentityEntities.User> _userManager;
+
+        public DeleteUserCommandHandler(UserManager<Domain.IdentityEntities.User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Unit> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _userManager.FindByIdAsync(request.Id);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(User), request.Id);
+            }
+
+            await _userManager.DeleteAsync(entity);
+
+            return Unit.Value;
+        }
+    }
+}
