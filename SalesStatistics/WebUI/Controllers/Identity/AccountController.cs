52fca5c0edb3712089fe23af37176f6ef7880ef3
Nik.Brosko <Nikita.brosko@mail.ru>
2022-02-03 23:53:30 +0300

    Create UserController Move AccountController to another directory

---
+++ SalesStatistics/WebUI/Controllers/Identity/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Application.UseCases.Identity.User.Commands.CreateUser;
+using Application.UseCases.Identity.User.Queries.LoginUser;
+using Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers.Identity
+{
+    public class AccountController : ApiControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromForm] CreateUserCommand command)
+        {
+            var result = await Mediator.Send(command);
+
+            if (result.Result.Succeeded)
+            {
+                await _signInManager.SignInAsync(result.User, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View("IdentityError", result.Result.Errors);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginUserQuery {ReturnUrl = returnUrl});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromForm] LoginUserQuery query)
+        {
+            var result = await Mediator.Send(query);
+
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(query.ReturnUrl) && Url.IsLocalUrl(query.ReturnUrl))
+                {
+                    return Redirect(query.ReturnUrl);
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(query);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ SalesStatistics/WebUI/Controllers/Identity/UserController.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Application.UseCases.Identity.User.Commands.CreateUser;
+using Application.UseCases.Identity.User.Commands.DeleteUser;
+using Application.UseCases.Identity.User.Commands.UpdateUser;
+using Application.UseCases.Identity.User.Queries.GetUsersWithPagination;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers.Identity
+{
+    public class UserController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> UsersPage([FromQuery] GetUsersWithPaginationQuery query)
+        {
+            return View(await Mediator.Send(query));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] CreateUserCommand command)
+        {
+            await Mediator.Send(command);
+
+            return RedirectToAction("UsersPage");
+        }
+
+        [HttpGet]
+        public IActionResult Update([FromRoute] string id)
+        {
+            return View(id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update([FromForm] UpdateUserCommand command)
+        {
+            await Mediator.Send(command);
+
+            return RedirectToAction("UsersPage");
+        }
+
+        [HttpDelete("{id}")]
+        [Route("Delete/{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            await Mediator.Send(new DeleteUserCommand {Id = id});
+
+            return RedirectToAction("UsersPage");
+        }
+    }
+}
