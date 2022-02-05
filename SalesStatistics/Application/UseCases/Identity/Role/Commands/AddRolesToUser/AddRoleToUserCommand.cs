5e740ece53d9eef73e802d3b6db52fa1f6cb13ef
Nik.Brosko <Nikita.brosko@mail.ru>
2022-02-05 23:58:02 +0300

    Create AddRoleToUserCommand use case

---
+++ SalesStatistics/Application/UseCases/Identity/Role/Commands/AddRolesToUser/AddRoleToUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.AddRolesToUser
+{
+    public class AddRoleToUserCommand : IRequest<IdentityResult>
+    {
+        public string UserId { get; set; }
+
+        public string RoleId { get; set; }
+    }
+}
---
+++ SalesStatistics/Application/UseCases/Identity/Role/Commands/AddRolesToUser/AddRoleToUserCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.AddRolesToUser
+{
+    public class AddRoleToUserCommandHandler<TUser> : IRequestHandler<AddRoleToUserCommand, IdentityResult> where TUser : class
+    {
+        private readonly UserManager<TUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AddRoleToUserCommandHandler(UserManager<TUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityResult> Handle(AddRoleToUserCommand request, CancellationToken cancellationToken)
+        {
+            var userEntity = await _userManager.FindByIdAsync(request.UserId);
+
+            if (userEntity is null)
+            {
+                throw new NotFoundException(nameof(TUser), request.UserId);
+            }
+
+            var roleEntity = await _roleManager.FindByIdAsync(request.RoleId);
+
+            if (roleEntity is null)
+            {
+                throw new NotFoundException(nameof(IdentityRole), request.RoleId);
+            }
+
+            return await _userManager.AddToRoleAsync(userEntity, roleEntity.Name);
+        }
+    }
+}
