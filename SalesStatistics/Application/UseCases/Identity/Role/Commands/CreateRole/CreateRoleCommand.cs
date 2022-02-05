de9d3c42686d8bd52cc4218aee3f242ac96ff7c9
Nik.Brosko <Nikita.brosko@mail.ru>
2022-02-05 23:57:27 +0300

    Create CreateRoleCommand use case

---
+++ SalesStatistics/Application/UseCases/Identity/Role/Commands/CreateRole/CreateRoleCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.CreateRole
+{
+    public class CreateRoleCommand : IRequest<IdentityResult>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ SalesStatistics/Application/UseCases/Identity/Role/Commands/CreateRole/CreateRoleCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.CreateRole
+{
+    public class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommand, IdentityResult>
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public CreateRoleCommandHandler(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityResult> Handle(CreateRoleCommand request, CancellationToken cancellationToken)
+        {
+            var role = new IdentityRole(request.Name);
+
+            var result = await _roleManager.CreateAsync(role);
+
+            return result;
+        }
+    }
+}
