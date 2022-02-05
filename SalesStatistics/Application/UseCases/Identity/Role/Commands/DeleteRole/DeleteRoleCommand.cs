64b5a65cd2b4938e649c9ca72656fff3010279ba
Nik.Brosko <Nikita.brosko@mail.ru>
2022-02-05 23:57:48 +0300

    Create DeleteRole use case

---
+++ SalesStatistics/Application/UseCases/Identity/Role/Commands/DeleteRole/DeleteRoleCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.DeleteRole
+{
+    public class DeleteRoleCommand : IRequest<IdentityResult>
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ SalesStatistics/Application/UseCases/Identity/Role/Commands/DeleteRole/DeleteRoleCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.DeleteRole
+{
+    public class DeleteRoleCommandHandler : IRequestHandler<DeleteRoleCommand, IdentityResult>
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DeleteRoleCommandHandler(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityResult> Handle(DeleteRoleCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _roleManager.FindByIdAsync(request.Id);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(IdentityRole), request.Id);
+            }
+
+            var result = await _roleManager.DeleteAsync(entity);
+
+            return result;
+        }
+    }
+}
