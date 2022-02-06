33b59edc02c07f9b6210565ee4d028a7a6579427
Nik.Brosko <Nikita.brosko@mail.ru>
2022-02-06 20:11:37 +0300

    Create RolesInitializer and UsersInitializer for initialize identity db with some basic roles and user "admin"

---
+++ SalesStatistics/Infrastructure/IdentityPersistence/Initializers/RolesInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.IdentityPersistence.Initializers
+{
+    public static class RolesInitializer
+    {
+        public static async Task InitializeAsync(RoleManager<IdentityRole> roleManager)
+        {
+            if (await roleManager.FindByNameAsync("admin") is null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+
+            if (await roleManager.FindByNameAsync("user") is null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+        }
+    }
+}
---
+++ SalesStatistics/Infrastructure/IdentityPersistence/Initializers/UsersInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.IdentityPersistence.Initializers
+{
+    public static class UsersInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager)
+        {
+            if (await userManager.FindByNameAsync("admin") is null)
+            {
+                var entity = new User
+                {
+                    UserName = "admin",
+                    Email = "Nikita.brosko@mail.ru"
+                };
+
+                await userManager.CreateAsync(entity, "Admin123");
+
+                await userManager.AddToRoleAsync(entity, "admin");
+            }
+        }
+    }
+}
