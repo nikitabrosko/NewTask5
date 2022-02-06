97995cd22d9d7f6499857d0cdc465d285702e120
Nik.Brosko <Nikita.brosko@mail.ru>
2022-02-06 17:08:02 +0300

    Add synchronously method Create to PaginatedList class

--- SalesStatistics/Application/Common/Models/PaginatedList.cs
+++ SalesStatistics/Application/Common/Models/PaginatedList.cs
@@ -35,5 +35,13 @@ namespace Application.Common.Models
 
             return new PaginatedList<T>(items, count, pageNumber, pageSize);
         }
+
+        public static PaginatedList<T> Create(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+
+            return new PaginatedList<T>(items, count, pageNumber, pageSize);
+        }
     }
 }
