f65afae5dcb24fd736dddafcd95f0fd4b35b2cb1
Nik.Brosko <Nikita.brosko@mail.ru>
2022-02-06 20:12:50 +0300

    Update Program class

--- SalesStatistics/WebUI/Program.cs
+++ SalesStatistics/WebUI/Program.cs
@@ -1,13 +1,32 @@
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Domain.IdentityEntities;
+using Infrastructure.IdentityPersistence.Initializers;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace WebUI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                var userManager = services.GetRequiredService<UserManager<User>>();
+
+                await RolesInitializer.InitializeAsync(roleManager);
+                await UsersInitializer.InitializeAsync(userManager);
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
