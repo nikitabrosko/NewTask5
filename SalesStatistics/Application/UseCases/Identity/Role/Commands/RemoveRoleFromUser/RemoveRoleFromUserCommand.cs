cf3799512ae70903eca8a43c2d1c25f275e2a68d
Nik.Brosko <Nikita.brosko@mail.ru>
2022-02-06 17:08:32 +0300

    Create RemoveRoleFromUserCommand use case

---
+++ SalesStatistics/Application/UseCases/Identity/Role/Commands/RemoveRoleFromUser/RemoveRoleFromUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.RemoveRoleFromUser
+{
+    public class RemoveRoleFromUserCommand : IRequest<IdentityResult>
+    {
+        public string UserId { get; set; }
+
+        public string RoleName { get; set; }
+    }
+}
---
+++ SalesStatistics/Application/UseCases/Identity/Role/Commands/RemoveRoleFromUser/RemoveRoleFromUserCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.UseCases.Identity.Role.Commands.RemoveRoleFromUser
+{
+    public class RemoveRoleFromUserCommandHandler : IRequestHandler<RemoveRoleFromUserCommand, IdentityResult>
+    {
+        private readonly UserManager<Domain.IdentityEntities.User> _userManager;
+
+        public RemoveRoleFromUserCommandHandler(UserManager<Domain.IdentityEntities.User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> Handle(RemoveRoleFromUserCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _userManager.FindByIdAsync(request.UserId);
+
+            if (entity is null)
+            {
+                throw new NotFoundException(nameof(Domain.IdentityEntities.User), request.UserId);
+            }
+
+            return await _userManager.RemoveFromRoleAsync(entity, request.RoleName);
+        }
+    }
+}
